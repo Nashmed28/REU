5bc597611b0d67665bb33444bba8a9d2415b7b1d
Nabib Ahmed <nahmed3536@gmail.com>
2016-06-24 13:30:02 -0400

    Scroll, sidebar, tab works

--- interface.js
+++ interface.js
@@ -2,6 +2,14 @@ var height = document.getElementById('mainbar').offsetHeight;
 
 
 $(document).ready(function () {	
+	// sidebar action, with toggle and text-switch
+	$("#sidebar-toggle").click(function() {
+        $(this).text(function(i, v){
+        	return v === '>>' ? '<<' : '>>'
+        });
+        $("#wrapper").toggleClass("toggled");
+    });
+    
 	// alert(height);
 	// document.getElementById('header-wrap').style.height = '90px';
 	// $(function() {
