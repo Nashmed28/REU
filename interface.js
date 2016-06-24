18d1a70f5b2dbf15cb9e646a39fcc392694dc76d
Nabib Ahmed <nahmed3536@gmail.com>
2016-06-24 00:55:58 -0400

    New Design2

--- interface.js
+++ interface.js
@@ -2,3 +2,16 @@
 //     var height = document.getElementById('header-wrap').offsetHeight;
 //     alert(height);
 // }
+$(document).ready(function () {	
+	// $(function() {
+	// 	$('#wrapper').toggleClass('toggled');
+	// });
+ //    $('#additional-parameters').click(function(e) {
+ //        e.preventDefault();
+ //        $('#wrapper').toggleClass('toggled');
+    });
+    // $("#menu-toggle").click(function(e) {
+    //     e.preventDefault();
+    //     $("#wrapper").toggleClass("toggled");
+    // });
+});
