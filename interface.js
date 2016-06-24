60665afcdebf7e95f61bfb6a1ec6606f00fe292a
Nabib Ahmed <nahmed3536@gmail.com>
2016-06-24 10:28:41 -0400

    Header Looks Gucci

--- interface.js
+++ interface.js
@@ -1,15 +1,16 @@
-// function myFunction() {
-//     var height = document.getElementById('header-wrap').offsetHeight;
-//     alert(height);
-// }
+var height = document.getElementById('mainbar').offsetHeight;
+
+
 $(document).ready(function () {	
+	// alert(height);
+	// document.getElementById('header-wrap').style.height = '90px';
 	// $(function() {
 	// 	$('#wrapper').toggleClass('toggled');
 	// });
  //    $('#additional-parameters').click(function(e) {
  //        e.preventDefault();
  //        $('#wrapper').toggleClass('toggled');
-    });
+    // });
     // $("#menu-toggle").click(function(e) {
     //     e.preventDefault();
     //     $("#wrapper").toggleClass("toggled");
